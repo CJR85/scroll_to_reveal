3242995278e0f52ff96bc922a6652cbc9e1507ea
Calvin Roberts <roberts.calvin85@gmail.com>
2023-09-06 00:10:03 +0100

    Create functions to add random colors to each box

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 'use strict';
+// Create boxes
 for (let i = 1; i <= 60; i++) {
   let box = document.createElement('div');
   box.classList.add('box');
   document.querySelector('.container').appendChild(box);
 }
 
+// Add random colors in each box
+let randomColorBlock = document.querySelectorAll('.box');
+
+function addColor() {
+  randomColorBlock.forEach((e) => {
+    e.style.background = randomColor();
+  });
+}
+function randomColor() {
+  let chars = '123456789abcdef';
+  let colorLength = 6;
+  let color = '';
+
+  for (let i = 1; i <= colorLength; i++) {
+    let randomColors = Math.floor(Math.random() * chars.length);
+    color += chars.substring(randomColors, randomColors + 1);
+  }
+  return '#' + color;
+}
+
+addColor();
+
+// Scroll Trigger function
 let boxes = document.querySelectorAll('.box');
 function scrollTrigger() {
   boxes.forEach((boxxx) => {
