b477926ce2a9e15448ea13135262e55e814e344b
Calvin Roberts <roberts.calvin85@gmail.com>
2023-09-05 23:49:10 +0100

    Create scrollTrigger function

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ for (let i = 1; i <= 60; i++) {
   box.classList.add('box');
   document.querySelector('.container').appendChild(box);
 }
+
+let boxes = document.querySelectorAll('.box');
+function scrollTrigger() {
+  boxes.forEach((boxxx) => {
+    if (boxxx.offsetTop < window.scrollY) {
+      boxxx.classList.add('active');
+    } else {
+      boxxx.classList.remove('active');
+    }
+  });
+}
+window.addEventListener('scroll', scrollTrigger);
